684f31f43b112e6e25e1506210d68003149a58af
zj03134 <zhangjie131424@163.com>
2021-07-08 18:31:08 +0800

    新建登录分支，登录组件，配置路由

---
+++ src/components/login/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>登录</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+
+</script>
+<style lang='' scoped>
+</style>
